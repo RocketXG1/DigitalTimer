484fab37dd34fc6c423007bca0f338a7ca817fe1
RocketXG1 <100332266+RocketXG1@users.noreply.github.com>
2026-04-10 22:30:03 -0600

    Add three output selectors below alarm time picker

--- app/src/main/java/com/example/digitaltimer/fragment_SetAlarm.kt
+++ app/src/main/java/com/example/digitaltimer/fragment_SetAlarm.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 class fragment_SetAlarm : Fragment() {
 
 
@@ -19,5 +20,13 @@ class fragment_SetAlarm : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment__set_alarm, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Spinner>(R.id.ID_OutputSelector1).setSelection(2)
+        view.findViewById<Spinner>(R.id.ID_OutputSelector2).setSelection(2)
+        view.findViewById<Spinner>(R.id.ID_OutputSelector3).setSelection(2)
     }
 }
