5c0ecdae7c3811d1c92d5480bb2a4a5972044ddf
RoketXG1 <rocket.xg@gmail.com>
2026-04-10 23:19:53 -0600

    SetAlarm fragment

--- app/src/main/java/com/example/digitaltimer/fragment_SetAlarm.kt
+++ app/src/main/java/com/example/digitaltimer/fragment_SetAlarm.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
+
 class fragment_SetAlarm : Fragment() {
 
 
@@ -19,5 +21,11 @@ class fragment_SetAlarm : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment__set_alarm, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val timePicker = view.findViewById<TimePicker>(R.id.ID_TimePicker24h)
+        timePicker.setIs24HourView(true)
     }
 }
